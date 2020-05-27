56fafe74cfff4d30eee71a53d17e91d8b7a755aa
Ben <russo588@gmail.com>
2020-05-27 19:23:44 -0400

    changed script on buffons needle

--- teaching/samplescripts/buffonsneedle_example.m
+++ teaching/samplescripts/buffonsneedle_example.m
@@ -1,13 +1,16 @@
 %assume the needle is of unit length.
 %floor cracks are 2 units apart.
 clear;
-A=[];
-c = struct('rr', [0.7, 0.0, 0.0], 'bb', [0,0,.7]); %defining custom colors. 
-n=10000000; %set the number of needles
-r_1=2*rand(n,2)-1; %generates random endpoints of needle in [-1,1]x[-1,1].
-theta=2*pi*rand(n,1); %generates random rotation angle for needle. 
-r_2=r_1+[cos(theta),sin(theta)]; %find the other endpoint one unit length away at the angle theta. 
 
+c = struct('rr', [0.7, 0.0, 0.0], 'bb', [0,0,.7]); %defining custom colors. 
+
+n=10000000; %set the number of needles
+
+r_1=2*rand(n,2)-1; %generates random endpoints of needle in [-1,1]x[-1,1].
+
+theta=2*pi*rand(n,1); %generates random rotation angle for needle. 
+
+r_2=r_1+[cos(theta),sin(theta)]; %find the other endpoint one unit length away at the angle theta. 
 
 % Helps find sign changes.
 s=r_1.*r_2; 
@@ -26,29 +29,12 @@ for i=1:min(500,n)
         plot([r_1(i,1); r_2(i,1)],[r_1(i,2);r_2(i,2)], 'Color', c.bb);
         hold on;
     end
-    %calculating pi from the number of crossings. 
-    S=sign(s);
-    crossings=sum(S(1:i,2)==-1);
-    approx=i/crossings;
-    str=strcat("Number of needles: ",num2str(i),",  ","approximation: ",num2str(approx));
-    %A=[]; %dirty memory trick: uncomment for the first run, then run again
-    %with comment on
-    delete(A);
-    A=annotation('textbox', [.15 .6 .3 .3], 'String', str, 'FitBoxToText','on' );
-    F=getframe;
-    im=frame2im(F);
-    [imind,cm]=rgb2ind(im,256);
-    if i==1
-        imwrite(imind,cm,'needle.gif','gif','Loopcount',inf, 'delaytime',.2);
-    else
-        imwrite(imind,cm,'needle.gif','gif','WriteMode','append','delaytime',.2);
-    end
+
 end
 
-figure
-imshow(F.cdata)
 
 
 %calculating pi from the number of crossings. 
+S=sign(s);
 final_crossings=sum(S(1:end,2)==-1);
 final_approx=n/final_crossings;
