cd1f24c12e07c2b9f825da455e4548a12b988620
Ben <russo588@gmail.com>
2020-07-24 13:37:36 -0400

    added monte

---
+++ teaching/samplescripts/montecarlo.py
@@ -0,0 +1,27 @@
+
+import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def f(x):
+	return math.exp(x)
+
+n=100 #number of samples	
+
+x=np.linspace(0,1,50)
+
+Domain =[i for i in x]
+Range =[f(i) for i in x]
+Sample=[random.random() for i in range(n)] 
+FSample=[f(i) for i in Sample]
+
+
+Average=sum(FSample)/n
+print(Average)
+
+scatter = plt.scatter(Sample, FSample, color='red') 
+
+plt.plot(Domain, Range)	
+plt.show()
--- teaching/scripts/matrixexample.m
+++
@@ -1,8 +0,0 @@
-A=[.5,-.6;.75,1.1]; 
-x=[2;0];
-for n= 1:100
-    x= A*x;
-    disp(x);
-    hold on;
-    plot (x(1),x(2), 'sk');
-end
